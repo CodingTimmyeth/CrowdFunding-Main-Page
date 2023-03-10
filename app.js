37b935e1cba7da6dd3361f59dadbffa59ba9c92b
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-03-09 21:27:48 -0800

    Finsihed Projectgit add .

--- app.js
+++ app.js
@@ -1,159 +1,250 @@
-const hamburgerDiv = document.getElementById('hamburger')
-const ham = document.getElementById('menu')
+// Overlay
+const overlay = document.createElement('div')
+const overlay2 = document.createElement('div')
+const overlay3 = document.createElement('div')
+
+
+// Hamburger Menu Variables
+// Hamburger Icons & Div Variables
+const hamburgerDiv = document.getElementById('hamburgerDiv');
+const Hamburger = document.getElementById('hamburger')
 const xmark = document.getElementById('xmark')
+const mainNav = document.getElementById('main-nav')
+// Nav Menu
 const mobileMenu = document.getElementById('mobile-menu')
 
-// Buttons
+// Function for the Hamburger Menu
+function hamburgerMenu() {
+  Hamburger.classList.toggle('close')
+  mobileMenu.classList.toggle('open')
+  xmark.classList.toggle('open')
 
-const btns = document.querySelectorAll('button')
+  // Applys the overlay effect
+  if (mobileMenu.classList.contains('open')) {
+    overlay.classList.add('overlay')
+    document.body.append(overlay)
+  } else {
+    overlay.remove()
+  }
+}
+hamburgerDiv.addEventListener('click', hamburgerMenu)
 
-// Main Container
-const mainCon = document.getElementById('main-container')
-// Second Container
-const modalMenu = document.getElementById('project-donation-menu')
-const closeModal = document.getElementById('close-modal')
 
+// Modal Menu Variables
+const modalMenuDiv = document.getElementById('modal-menu')
+const xmarkModal = document.getElementById('close-modal')
 
+// Open Modal - Select Rewards Buttons
+const rewardBtns = document.querySelectorAll('button[value="modal"]')
 
-btns.forEach((button, index) =>  {
-    button.addEventListener('click', function () {
-        if(index === btns.length -1) {
-            alert('Sorry, this is out of stock')
-        } else {
-            console.log('this is not the last button');
-            mainCon.classList.toggle('open')
-            modalMenu.classList.remove('close')
-            closeModal.addEventListener('click', function() {
-                modalMenu.classList.add('close')
-                console.log('closed');
-            })
-        }
+// For Each button, add a event listener to open the modal then add
+// a exit to exit out the modal, make sure to add the overlay
+rewardBtns.forEach(button => {
+  button.addEventListener('click', function () {
+    modalMenuDiv.classList.toggle('open')
+    if (modalMenuDiv.classList.contains('open')) {
+      // Add the second overlay here
+      overlay2.classList.add('overlay-two')
+      document.body.append(overlay2)
+    }
+
+    // Close Modal
+    xmarkModal.addEventListener('click', function () {
+      modalMenuDiv.classList.remove('open')
+      overlay2.remove()
     })
+  })
 })
 
-hamburgerDiv.addEventListener('click', showMenu);
+// Input Radio Variables
+const inputRadios = document.querySelectorAll('input[type="radio"]')
 
-function showMenu () {
-    ham.classList.toggle('close')
-    xmark.classList.toggle('open')
-    mobileMenu.classList.toggle('open')
-    mobileMenu.classList.toggle('menu-bg')
-}
+// Containers for Modal Content
+const firstCon = document.getElementById('first-con')
+const secondCon = document.getElementById('second-con')
+const thirdCon = document.getElementById('third-con')
 
-// Function to check if input radio is Active 
-// function inputActive () {
-//     const inputRadio = document.querySelectorAll('input[type="radio"]')
-//     const firstCon = document.getElementById('first-con')
-//     const secondCon = document.getElementById('second-con')
-//     const thirdCon = document.getElementById('third-con')
-//     const pledgeAmount = document.getElementById('pledge-amount')
+// Pledge Amount Variables
+const noPledge = document.getElementById('no-pledge')
+const bambooPledge = document.getElementById('bamboo-pledge')
+const blackEdition = document.getElementById('black-edition')
 
-//     inputRadio.forEach((input) => {
-//         input.addEventListener('click', function() {
-//             if(input.value === 'no-reward'){
-//                 firstCon.classList.add('active')
-//                 secondCon.classList.remove('active')
-//                 thirdCon.classList.remove('active')
-//             } else if(input.value === 'bamboo-stand'){
-//                 secondCon.classList.add('active')
-//                 firstCon.classList.remove('active')
-//                 thirdCon.classList.remove('active')
-//             } else if(input.value === 'black-edition'){
-//                 thirdCon.classList.add('active')
-//                 secondCon.classList.remove('active')
-//                 firstCon.classList.remove('active')
+// Current Backers Variable
+let currentBackersDiv = document.getElementById('current-backers')
+let currentBackers = localStorage.getItem('currentBackers') || 0
+currentBackersDiv.innerText = currentBackers
 
-//             }
-//         })
-//     })
-// }
+// Current Price Variable
+let currentPriceDiv = document.getElementById('current-price')
+let currentPrice = Number(localStorage.getItem('currentPrice')) || 0;
+currentPriceDiv.innerText = currentPrice
 
-// inputActive()
-
-function inputActive () {
-    const inputRadio = document.querySelectorAll('input[type="radio"]')
-    const firstCon = document.getElementById('first-con')
-    const secondCon = document.getElementById('second-con')
-    const thirdCon = document.getElementById('third-con')
-
-    // Pledge Amount
-    const bambooPledge = document.getElementById('bamboo-pledge')
-    const blackEdition = document.getElementById('black-edition')
-    const noPledge = document.getElementById('no-pledge')
-  
-    let activeCon = null;
-  
-    // Add click event listeners to each container element
-    firstCon.addEventListener('click', function() {
-      setActiveCon(firstCon);
-      noPledge.classList.add('open')
-    });
-    secondCon.addEventListener('click', function() {
-      setActiveCon(secondCon);
-      bambooPledge.classList.add('open')
-    });
-    thirdCon.addEventListener('click', function() {
-      setActiveCon(thirdCon);
-      blackEdition.classList.add('open')
-    });
-  
-    // Function to set the active container
-    function setActiveCon(con) {
-      // Remove the "active" class from the previous container
-      if (activeCon) {
-        activeCon.classList.remove('active');
-        bambooPledge.classList.remove('open')
-        noPledge.classList.remove('open')
-        blackEdition.classList.remove('open')
-      }
-  
-      // Add the "active" class to the new container
-      con.classList.add('active');
-      activeCon = con;
+// Checks if the input radio is checked and checks the value if it contains
+// the correspoding value if true add the classlist 'active' and remove
+// all other active classes, also show pledge amount div for each container
+inputRadios.forEach(input => {
+  input.addEventListener('click', function(){
+    if(input.checked && input.value === 'no-reward'){
+      firstCon.classList.add('active')
+      secondCon.classList.remove('active')
+      thirdCon.classList.remove('active')
+      noPledge.classList.toggle('open')
+    } else if(input.checked && input.value === 'bamboo-stand'){
+      secondCon.classList.add('active')
+      firstCon.classList.remove('active')
+      thirdCon.classList.remove('active')
+      bambooPledge.classList.toggle('open')
+    } else if(input.checked && input.value === 'black-edition'){
+      thirdCon.classList.add('active')
+      secondCon.classList.remove('active')
+      firstCon.classList.remove('active')
+      blackEdition.classList.toggle('open')
+    } else{
+      secondCon.classList.remove('active')
+      thirdCon.classList.remove('active')
+      firstCon.classList.remove('active')
     }
-  
-    inputRadio.forEach((input) => {
-      input.addEventListener('click', function() {
-        if (input.value === 'no-reward') {
-          setActiveCon(firstCon);
-        } else if (input.value === 'bamboo-stand') {
-          setActiveCon(secondCon);
-        } else if (input.value === 'black-edition') {
-          setActiveCon(thirdCon);
-        }
-      })
-    })
+  })
+})
+
+// const inputText = document.querySelectorAll('input[type="text"]')
+const inputOne = document.querySelector('input[type="text"]:first-child')
+const pledgeSubmitBtns = document.querySelectorAll('button[type="submit"]')
+
+
+const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+
+pledgeSubmitBtns.forEach(btn => {
+  btn.addEventListener('click', function(){
+
+
+    if(btn.value === 'none'){
+      console.log('Current:', btn);
+      const pass = checkEmail(inputOne.value, 'Please Enter valid email!')
+      if(pass === true){
+        thankYouPage.classList.add('open')
+        // Add Overlay for the Thank You Page
+        overlay3.classList.add('overlay-three')
+        document.body.append(overlay3)
+        modalMenuDiv.classList.toggle('close')
+
+        currentBackers++;
+        currentBackersDiv.innerText = currentBackers;
+        // Store the updated value in LocalStorage
+        localStorage.setItem('currentBackers', currentBackers)
+      }
+    }
+  })
+})
+
+// Pledge Amount for Bamboo Stand
+  // Variables
+  const bambooPledgeAmount = document.querySelector('#bamboo-pledge-amount')
+  const bambooBtn = document.getElementById('bambooBtn')
+  const secondErr = document.getElementById('blankerr')
+
+
+// Pledge Amount for Black Editon and Update the DOM functionality
+  // Variables
+  const blackPledgeAmount = document.querySelector('#pldege-black-amount')
+  const blackBtn = document.getElementById('blackBtn')
+  const thirdErr = document.getElementById('blankerr')
+
+  function checkAmount (userInput, div, amount) {
+    if(userInput === '' || userInput < amount) {
+      console.log('empty, input');
+      div.classList.add('red')
+    } else {
+      console.log('passed');
+      console.log('show thank you page and update the DOM');
+      div.classList.remove('red')
+
+      thankYouPage.classList.add('open')
+      // Add Overlay for the Thank You Page
+      overlay3.classList.add('overlay-three')
+      document.body.append(overlay3)
+      modalMenuDiv.classList.toggle('close')
+
+      // Update the DOM 
+      currentBackers++;
+      currentBackersDiv.innerText = currentBackers;
+       // Store the updated value in LocalStorage
+      localStorage.setItem('currentBackers', currentBackers)
+
+      // DOM - Current Price
+      currentPrice = parseInt(localStorage.getItem('currentPrice')) || 0;
+      currentPrice += parseInt(userInput)
+      currentPriceDiv.innerText = currentPrice;
+      // Store the updated Price
+      localStorage.setItem('currentPrice', currentPrice)
+    }
+    console.log(`userInput: ${userInput}, currentPrice: ${currentPrice}`);
+
   }
   
-  inputActive()
+// EventListeners for the pledge amounts
+  blackBtn.addEventListener('click', function() {
+    checkAmount(blackPledgeAmount.value, blackPledgeAmount, 75)
+  })
 
-function pledgeAmount () {
-    const submitBtns = document.querySelectorAll('button[type="submit"]');
-    const inputText = document.querySelectorAll('input[type="text"]');
+  bambooBtn.addEventListener('click', function() {
+    checkAmount(bambooPledgeAmount.value, bambooPledgeAmount, 25)
+  })
 
-    submitBtns.forEach(button => {
-        button.addEventListener('click', function (event){
-            event.preventDefault()
+// Function checking Email
+function checkEmail(Email, error) {
+  const errMessage = document.getElementById('errMessage')
 
-            let emptyFields = false;
+  let pass = true;
 
-            // loop through the input fields
-            inputText.forEach(input => {
-                if(input.value.trim() === '') { // check if input field is empty 
-                    emptyFields = true;
-                } 
-            });
-
-            if(emptyFields) {
-                console.log("Can't leave blank!");
-            } else {
-                console.log("Thank you Message!");
-            }
-        })
-    })
+  if(regex.test(Email)){
+    console.log('passed!');
+    pass = true
+    inputOne.classList.remove('red')
+    errMessage.innerText = ''
+  } else {
+    console.log('failed');
+    inputOne.classList.add('red')
+    errMessage.innerText = error
+    pass = false;
+  }
+  return pass
 }
-pledgeAmount()
 
 
+// Thank You Page 
+const thankYouPage = document.getElementById('thankYou')
 
-  
+// Close Thank You Btn Variable
+const gotItBtn = document.getElementById('gotItBtn')
+
+// function to remove all styling and take them back to the front page
+function closeThankYou () {
+  console.log('close thank you and the overlay');
+  thankYouPage.classList.toggle('open')
+  overlay3.remove()
+  overlay2.remove()
+  location.reload()
+
+}
+gotItBtn.addEventListener('click', closeThankYou)
+
+
+// BookMark
+
+const bookMarkIcon = document.getElementById('bookmarkIcon')
+const bookMarkText = document.getElementById('bookmarkText')
+
+
+bookMarkIcon.addEventListener('click', function () {
+  if (bookMarkIcon.classList.contains('clicked')) {
+    // If the button has already been clicked, remove the "ed" from the text content
+    bookMarkText.textContent = bookMarkText.textContent.replace('ed', '');
+    bookMarkIcon.classList.remove('clicked');
+  } else {
+    // If the button has not been clicked, add "ed" to the end of the text content
+    bookMarkText.textContent += 'ed';
+    bookMarkIcon.classList.add('clicked');
+  }
+})
